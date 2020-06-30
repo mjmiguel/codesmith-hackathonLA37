bd37aff51d760a79442ddaa875a6c9de278cae8f
Luis-KM-Lo <lo.kwun.man@gmail.com>
2020-06-29 22:12:29 -0700

    things don't work out the way it should :(

--- main.js
+++ main.js
@@ -13,15 +13,31 @@
 
 const pTags = document.querySelectorAll('p');
 
+// pTags.forEach((el) => {
+//   const newDiv = document.createElement('p');
+//   newDiv.innerHTML = 'NEVER GONNA GIVE YOU UP';
+//   el.appendChild(newDiv);
+// });
+
+function getInput() {
+  let variable = document.getElementById('input_id').value;
+  pTags.forEach((el) => {
+    const newDiv = document.createElement('p');
+    newDiv.innerHTML = variable;
+    el.appendChild(newDiv);
+  });
+}
+
 pTags.forEach((el) => {
   const newDiv = document.createElement('p');
-  newDiv.innerHTML = 'NEVER GONNA GIVE YOU UP';
+  newDiv.innerHTML = "never gonna give you up";
   el.appendChild(newDiv);
 });
 
-const aTags = document.querySelectorAll('a');
 
-aTags.forEach((el) => {
-  el.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-})
+// const aTags = document.querySelectorAll('a');
+
+// aTags.forEach((el) => {
+//   el.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+// })
 
